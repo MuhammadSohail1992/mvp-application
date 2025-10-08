0c1ae1582eaca4eff58696746652e7fb2a66899a
MuhammadSohail1992 <sohail.afraz@gmail.com>
2025-10-08 20:41:01 +0500

    added the profile page, need to set its tabs and sub tabs

--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 import { Card } from 'primeng/card';
+import { TabsModule } from 'primeng/tabs';
 
 @Component({
   selector: 'app-profile',
-  imports: [Card],
+  imports: [Card, TabsModule],
   templateUrl: './profile.html',
-  styleUrl: './profile.scss'
+  styleUrl: './profile.scss',
 })
 export class Profile {
+  userName = "Muhammad"
+  verticalTabs: MenuItem[] = [
+    { label: 'Releases', icon: 'pi pi-folder' },
+    { label: 'Videos', icon: 'pi pi-video' },
+    { label: 'Photos', icon: 'pi pi-image' },
+    { label: 'Events', icon: 'pi pi-calendar' },
+    { label: 'Activity Wall', icon: 'pi pi-comments' },
+  ];
 
+  // Active vertical tab
+  activeVertical: MenuItem = this.verticalTabs[0];
+
+  // Horizontal TabView index
+  activeTabIndex: number = 0;
 }
